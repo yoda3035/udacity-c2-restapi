74989b343ef4c66c09859e04dbe9415b3d6906c1
JJ <jenia.baghdanian@gmail.com>
2019-07-11 21:56:36 -0700

    Added edited .config

---
+++ config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": "us-west-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-baghdanian-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
